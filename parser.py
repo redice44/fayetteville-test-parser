f0db0833fc23a631cdbe1d3af9c1218f4e2f3295
Matt Thomson <red.cataclysm@gmail.com>
2016-06-03 17:15:45 -0400

    Handles splitting questions more accurately

--- parser.py
+++ parser.py
@@ -6,11 +6,13 @@ ansPrefix = ['0', 'a', 'b', 'c', 'd', 'e', 'f', 'g']
 testBank = sys.stdin.read()
 testBank = testBank.split('\n')
 testBank = "".join(testBank)
-testBank = re.split('\d+\. ', testBank)
+testBank = "a" + testBank
+testBank = re.split("OBJECTIVES:", testBank)
 testBank = [elem for elem in testBank if elem.find("a. ") != -1]
 
 for questionNum in range(len(testBank)):
   question = testBank[questionNum]
+  question = re.split('[a-zA-z]\d+\. ', question)[1]
   question = question[:question.find("POINTS:")]
   question = re.split("ANSWER:", question)
   answer = question[1].strip()
