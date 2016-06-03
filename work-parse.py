64249313f18fcbdab518ebedc0ac858adec5a78d
Matt Thomson <red.cataclysm@gmail.com>
2016-06-03 00:56:21 -0400

    Now handles true/false

--- work-parse.py
+++ work-parse.py
@@ -24,7 +24,7 @@ for questionNum in range(len(testBank)):
   print "%s. %s" % (questionNum+1, question[0])
   # Print answers with answer prefix
   for ans in range(1, len(question)):
-    if ansPrefix[ans] == answer:
+    if ansPrefix[ans] == answer or (ans == 1 and answer == "True") or (ans == 2 and answer == "False"):
       sys.stdout.write('*')
     print "%s. %s" % (ansPrefix[ans], question[ans])
   print "\n"
